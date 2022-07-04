1ebac2021a7a16206d8d43b3f2c44dc786f068f4
Maximilian Kuechen <maximilian.kuechen@gmail.com>
2022-07-04 17:59:07 -0400

    added file conversions

---
+++ server/converter.js
@@ -0,0 +1,62 @@
+import showdown from 'showdown'; // https://www.npmjs.com/package/showdown
+import docx_converter from 'mammoth'; // https://www.npmjs.com/package/pammoth
+import odt_converter from 'odt2html'; // https://www.npmjs.com/package/odt2html
+import img_converter from 'image-data-uri'; // https://www.npmjs.com/package/image-data-uri
+
+const md_converter = new showdown.Converter();
+md_converter.setOption('noHeaderId', true);
+
+export async function convert(file, type) {
+    switch (type) {
+        case 'md': 
+            return markdown(file);
+        case 'docx':
+            return await docx(file);
+        case 'odt':
+            return await odt(file);
+        case 'img':
+             return await img(file);
+        default: return -1;
+    }
+}
+
+function markdown(file) {
+    try {
+        let html =  md_converter.makeHtml(file);
+        return html;
+    } catch (err) {
+        console.log(error);
+        return -1;
+    }
+}
+
+async function docx(path) {
+    try {
+        let html = await docx_converter.convertToHtml({path: path});
+        return html.value;
+    } catch (err) {
+        console.log(err);
+        return -1;
+    }
+}
+
+async function odt(path) {
+    try {
+        let html = await odt_converter.toHTML({path: path});
+        console.log(html);
+    } catch (err) {
+        console.log(err);
+        return -1;
+    }
+}
+
+async function img(path) {
+    try {
+        let encoded = await img_converter.encodeFromFile(path);
+        let html_str = `<img src="${encoded}" alt="ADD AN ALT">`;
+        return html_str;
+    } catch (err) {
+        console.log(err);
+        return -1;
+    }
+}
